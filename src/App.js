52b5c611eebae92e3e6e674b2d59e81b8e9432fb
nisozakuto <nisozakuto@gmail.com>
2020-11-17 20:56:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+// see https://repl.it/@Dotdash/Goodreads-Server-Express for implementation details
+// const apiUrl = `https://goodreads-server-express--dotdash.repl.co/search/${term}`;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      term: "",
+      results: null,
+      selectedBookInfo: null,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  getDetails() {
+    fetch(
+      `https://goodreads-server-express--dotdash.repl.co/search/${this.state.term}`,
+      {
+        method: "GET",
+        mode: "cors",
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          results: res.list,
+        });
+      });
+  }
+
+  handleSubmit(event) {
+    this.getDetails();
+    event.preventDefault();
+  }
+
+  selectedBook(title) {
+    fetch(`https://goodreads-server-express--dotdash.repl.co/search/${title}`, {
+      method: "GET",
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          selectedBookInfo: res.list,
+        });
+      });
+  }
+
+  homepage() {
+    this.setState({
+      selectedBookInfo: null,
+      results: null,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <ul>
+            <li
+              onClick={() => {
+                this.homepage();
+              }}
+            >
+              goodreads API
+            </li>
+          </ul>
+        </header>
+        <main>
+          <section className="search">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                placeholder={"term"}
+                value={this.state.vinNumber}
+                onChange={this.handleChange}
+              />
+              <input type="submit" value="Search" />
+            </form>
+          </section>
+          <section>
+            <article className="books">
+              {this.state.selectedBookInfo ? (
+                <div className="book">
+                  <h1>{this.state.selectedBookInfo[0].authorName}</h1>
+                  <h3>{this.state.selectedBookInfo[0].title}</h3>
+                  <img
+                    src={this.state.selectedBookInfo[0].imageUrl}
+                    alt={this.state.selectedBookInfo[0].title}
+                    width="250px"
+                  ></img>
+                </div>
+              ) : (
+                <>
+                  {this.state.results ? (
+                    this.state.results.map((e) => (
+                      <div
+                        className="book"
+                        onClick={() => {
+                          this.selectedBook(e.title);
+                        }}
+                      >
+                        <h1>{e.authorName}</h1>
+                        <h3>{e.title}</h3>
+                        <img src={e.imageUrl} alt={e.title} width="250px"></img>
+                      </div>
+                    ))
+                  ) : (
+                    <p>Type your term and search!</p>
+                  )}
+                </>
+              )}
+            </article>
+          </section>
+        </main>
+      </>
+    );
+  }
 }
-
-export default App;
